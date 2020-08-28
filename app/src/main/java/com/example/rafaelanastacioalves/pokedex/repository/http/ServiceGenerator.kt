3a9a262e9e7370551e75cb8d5e608ba08669e797
rafaelanastacioalves <rafaelanastacioalves@gmail.com>
2020-08-28 18:48:46 -0300

    refactor: Improves Service Generator so one instance is created

--- app/src/main/java/com/example/rafaelanastacioalves/pokedex/repository/http/ServiceGenerator.kt
+++ app/src/main/java/com/example/rafaelanastacioalves/pokedex/repository/http/ServiceGenerator.kt
@@ -1,32 +1,25 @@
 package com.example.rafaelanastacioalves.pokedex.repository.http
 
 import com.example.rafaelanastacioalves.pokedex.BuildConfig
-
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 object ServiceGenerator {
-
-    private val appInterceptor = AppInterceptor()
-
-
-    fun <S> createService(serviceClass: Class<S>): S {
-
-        val builder = Retrofit.Builder()
-                .baseUrl(BuildConfig.API_BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-
-        val httpClient = OkHttpClient.Builder()
-
-        val interceptor = HttpLoggingInterceptor()
-
-        interceptor.level = HttpLoggingInterceptor.Level.BODY
-        val retrofit = builder.client(httpClient
-                .addInterceptor(interceptor)
-                .build()).build()
-        return retrofit.create(serviceClass)
+    private val httpClient = OkHttpClient.Builder()
+    private val logging = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+    private val retrofitBuilder: Retrofit.Builder = Retrofit.Builder()
+            .baseUrl(BuildConfig.API_BASE_URL)
+            .client(httpClient
+                    .addInterceptor(logging)
+                    .build())
+            .addConverterFactory(GsonConverterFactory.create())
+    private val retrofit = retrofitBuilder.build()
+    fun <S> createService(serviceClass: Class<S>?): S {
+        return retrofit
+                .create(serviceClass)
     }
-
 }
